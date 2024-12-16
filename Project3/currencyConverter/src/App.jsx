1447940266690531c574df548af5625325f2245d
vedkulk <kulkarnived3@gmail.com>
2024-12-16 20:42:23 +0530

    InputBox Component

--- Project3/currencyConverter/src/App.jsx
+++ Project3/currencyConverter/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="h-screen w-full bg-cyan-100 flex flex-wrap justify-center items-center bg-no-repeat bg-cover bg-center bg-[url('https://images.pexels.com/photos/164527/pexels-photo-164527.jpeg')]" >
+      <div className='text-cyan-500 text-5xl'>
+      HI
+      </div>
+    </div>
   )
 }
 
